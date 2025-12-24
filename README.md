fd3bd89fed7812b6423aed8735c3d9c204b14591
Terrunda <terrencegift49@gmail.com>
2025-12-24 09:59:44 +0300

    Initalised the necessary files for the Summative Lab project, from which each file (with the exception of syles.css) has a form of planned objective and functionality denoted in comments.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Worldly-SPA-Summative-Lab
+
+Task: Create a SPA that enables users to search for a word and retrieve information pertaining to that word.
+
+Expectation: Wordly, an online language learning platform is a `interactive dictionary feature`.
+
+Challenge: Accessing saved words and a user saving their favorite terms 
---
+++ workfiles/index.js
@@ -0,0 +1,15 @@
+const apiEndPoint = `https://api.dictionaryapi.dev/api/v2/entries/en/`
+
+
+async function handleFetch() {
+
+};
+
+function handleData(arrayOfObjectData) {
+
+};
+/* A function should be created to handle the response data.
+ For example, if the user searches a word that does not have a synonym, the placeholder element should respond with a `fallback message`.
+ Such a message could be 'No synonyms available' */
+
+ // Functionality to handle invalid inputs and empty search queries.
